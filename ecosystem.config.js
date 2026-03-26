51128c82a0c76f736bab45b35ac66817e1e8a245
shuflovic <pavel.skuta@gmail.com>
2026-03-26 11:36:43 +0100

    Add PM2 ecosystem config for Pi deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+{
+  "apps": [
+    {
+      "name": "tracks-identifier-backend",
+      "script": "server/index.js",
+      "cwd": "/home/pi/tracks_identifier",
+      "instances": 1,
+      "autorestart": true,
+      "watch": false,
+      "env": {
+        "NODE_ENV": "production"
+      }
+    },
+    {
+      "name": "tracks-identifier-frontend",
+      "script": "npm",
+      "args": "run dev",
+      "cwd": "/home/pi/tracks_identifier",
+      "instances": 1,
+      "autorestart": true,
+      "watch": false,
+      "env": {
+        "NODE_ENV": "production"
+      }
+    }
+  ]
+}
